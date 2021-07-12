6105a4c1a83600b80f92737f06c1385822f4ad40
segf00lt <joaodear2007@gmail.com>
2021-07-12 17:41:49 -0300

    wrote subtractor

--- ALU.py
+++ ALU.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 # Writing an ALU in python
-# Try to remember that functions operate on single bits
 
 from sys import argv
 
 def bin_input(n):
+    n = n 
     n = "{0:b}".format(n)
     n = [int(i) for i in n]
     return n
@@ -45,6 +45,36 @@ def NAND(a, b):
 def NOR(a, b):
     return ~(a | b)
 
+def MUX(a, b, control):
+    if NOT(control):
+        return a
+    else:
+        return b
+
+def HALF_SUB(a, b):
+    R = XOR(a, b)
+    C = AND(NOT(a), b)
+    return R, C
+
+def FULL_SUB(a, b, C_in):
+    R_1, C_1 = HALF_SUB(a, b)
+    R_2, C_2 = HALF_SUB(R_1, C_in)
+    R = R_2
+    C = C_1 | C_2
+    return R, C
+
+def SUBTRACTOR(a, b):
+    a, b = pad(a, b)
+
+    result = []
+    carry = 0
+    for i in reversed( range(len(a)) ):
+        bit_sum, carry = FULL_SUB(a[i], b[i], carry)
+        result.insert(0, bit_sum)
+    result.insert(0, carry)
+
+    return ''.join(str(i) for i in result)
+
 def HALF_ADD(a, b):
     R = XOR(a, b)
     C = AND(a, b)
@@ -70,7 +100,19 @@ def ADDER(a, b):
 
     return ''.join(str(i) for i in result)
 
+def ALU(A, B, control):
+    if control == 0:
+        R = ADDER(A, B)
+    elif control == 1:
+        R = SUBTRACTOR(A, B)
+    elif control == 2:
+        R = AND(A, B)
+    elif control == 3:
+        R = OR(A, B)
+
+    return R, Z, C, V
+
 if __name__ == '__main__':
     a = bin_input(int(argv[1]))
     b = bin_input(int(argv[2]))
-    print(ADDER(a, b))
+    print(SUBTRACTOR(a, b))
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ALU.py
 
-Thought I'd write an ALU today
+Thought I'd write an ALU today.
+I realize that I'm cheating a bit by not writing single bit gates... but come
+on it's still a good exercise... right?
