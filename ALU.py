53d667150e8841e1269b7f786da5e98c796714cf
segf00lt <joaodear2007@gmail.com>
2021-07-13 14:57:23 -0300

    cleaned up stuff

--- ALU.py
+++ ALU.py
@@ -4,80 +4,55 @@
 
 from sys import argv
 
+WORD = 16
+SIGN_BIT = 15
+
 def bin_input(n):
-    n = n 
     n = "{0:b}".format(n)
     n = [int(i) for i in n]
-    return n
-
-def pad(a, b):
-    l_a = len(a)
-    l_b = len(b)
-    dif = l_a - l_b
+    dif = 16 - len(n)
     if dif > 0:
         for i in range(dif):
-            b.insert(0, 0)
+            n.insert(0, 0)
     else:
-        for i in range(abs(dif)):
-            a.insert(0, 0)
-    return a, b
+        exit("Unsupported integer integer size")
+    return n
 
-def cfor(first, test, update):
-    while test(first):
-        yield first
-        first = update(first)
+def AND(A, B):
+    result = []
+    for i in reversed(range(WORD)):
+        result.insert(0, A[i] & B[i])
+    result = ''.join(str(i) for i in result)
 
-def AND(a, b):
-    return a & b
+    return result
 
-def OR(a, b):
-    return a | b
+def OR(A, B):
+    result = []
+    for i in reversed(range(WORD)):
+        result.insert(0, A[i] | B[i])
+    result = ''.join(str(i) for i in result)
 
-def XOR(a, b):
-    return (a | b) & (~a | ~b)
-
-def NOT(i):
-    return ~i
-
-def NAND(a, b):
-    return ~(a & b)
-
-def NOR(a, b):
-    return ~(a | b)
+    return result
 
 def MUX(a, b, control):
-    if NOT(control):
+    if ~control:
         return a
     else:
         return b
 
-def HALF_SUB(a, b):
-    R = XOR(a, b)
-    C = AND(NOT(a), b)
-    return R, C
+def OVERFLOW(a, b, r):
+    pair_1 = a & b
+    pair_2 = ~a & ~b
+    r_1 = ~r
+    r_2 = r
+    out_1 = pair_1 & r_1
+    out_2 = pair_2 & r_2
 
-def FULL_SUB(a, b, C_in):
-    R_1, C_1 = HALF_SUB(a, b)
-    R_2, C_2 = HALF_SUB(R_1, C_in)
-    R = R_2
-    C = C_1 | C_2
-    return R, C
-
-def SUBTRACTOR(a, b):
-    a, b = pad(a, b)
-
-    result = []
-    carry = 0
-    for i in reversed( range(len(a)) ):
-        bit_sum, carry = FULL_SUB(a[i], b[i], carry)
-        result.insert(0, bit_sum)
-    result.insert(0, carry)
-
-    return ''.join(str(i) for i in result)
+    return out_1 | out_2
 
 def HALF_ADD(a, b):
-    R = XOR(a, b)
-    C = AND(a, b)
+    R = a ^ b
+    C = a & b
     return R, C
 
 def FULL_ADD(a, b, C_in):
@@ -88,31 +63,26 @@ def FULL_ADD(a, b, C_in):
     C = C_1 | C_2
     return R, C
 
-def ADDER(a, b):
-    a, b = pad(a, b)
-
+def ADD_SUB(A, B, OP):
     result = []
-    carry = 0
-    for i in reversed( range(len(a)) ):
-        bit_sum, carry = FULL_ADD(a[i], b[i], carry)
+    carry = OP
+    bit_sum = 0
+    for i in reversed(range(WORD)):
+        bit_sum, carry = FULL_ADD(A[i], (OP ^ B[i]), carry)
         result.insert(0, bit_sum)
-    result.insert(0, carry)
 
-    return ''.join(str(i) for i in result)
+    result = ''.join(str(i) for i in result)
+
+    return result, OVERFLOW(A[WORD-1], B[WORD-1], bit_sum), carry
 
 def ALU(A, B, control):
-    if control == 0:
-        R = ADDER(A, B)
-    elif control == 1:
-        R = SUBTRACTOR(A, B)
-    elif control == 2:
-        R = AND(A, B)
-    elif control == 3:
-        R = OR(A, B)
 
     return R, Z, C, V
 
 if __name__ == '__main__':
     a = bin_input(int(argv[1]))
     b = bin_input(int(argv[2]))
-    print(SUBTRACTOR(a, b))
+    print(ADD_SUB(a, b, 0))
+    print(ADD_SUB(a, b, 1))
+    print(AND(a, b))
+    print(OR(a, b))
